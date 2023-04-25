f6bca3b2d2672a19d9a590059a72db5bab186587
NappOS <davioliveirasanto.work@gmail.com>
2023-04-25 07:35:31 -0300

    Fixing Q1

--- AEDS/exerciciosAeds/Exercicios collections/Exercicios 1-17/1/Questão1.cs
+++ AEDS/exerciciosAeds/Exercicios collections/Exercicios 1-17/1/Questão1.cs
@@ -8,71 +8,57 @@ namespace ListaColections
         public static void Main(string[] args)
         {
             Al();
-            // fila();
-            // pilha();
+            fila();
+            pilha();
         }
 
         static void Al()
         {
             ArrayList AL = new ArrayList();
-            for (int i = 1; i <= 26; i++)
+            for (int i = 0; i < 10; i++)
             {
                 AL.Add(i);
             }
             //Imprimindo
-            Console.Write("\nNúmeros de 1 a 25:");
+            Console.WriteLine("ArrayList:");
             foreach (int n in AL)
             {
-                Console.Write(AL[i] + " ");
+                Console.Write(n + " ");
             }
-
-            //Imprimindo Invertido
-            Console.WriteLine("\nNúmeros de 25 a 1:");
-            for (int i = AL.Count - 1; i >= 0; i--)
-            {
-                Console.Write(AL[i] + " ");
-            }
+             Console.WriteLine();
         }
 
         static void fila()
         {
             Queue fila = new Queue();
-            for (int i = 1; i <= 26; i++)
+            for (int i = 0; i < 10; i++)
             {
                 fila.Enqueue(i);
             }
             //Imprimindo
-            Console.Write("\nNúmeros de 1 a 25:");
+             Console.WriteLine("Fila:");
             foreach (int n in fila)
             {
-                Console.Write(fila[i] + " ");
+                Console.Write(n + " ");
             }
-            //Imprimindo Invertido
-            Console.WriteLine("\nNúmeros de 25 a 1:");
-            for (int i = fila.Count - 1; i >= 0; i--)
-            {
-                Console.Write(fila[i] + " ");
-            }
+            Console.WriteLine();
         }
 
         static void pilha()
         {
             Stack pilha = new Stack();
-            for (int i = 1; i <= 26; i++)
+            for (int i = 0; i < 10; i++)
             {
                 pilha.Push(i);
             }
-            Console.Write("\nNúmeros de 1 a 25:");
+            //Imprimindo
+             Console.WriteLine("Pilha:");
             foreach (int n in pilha)
             {
-                Console.Write(pilha[i] + " ");
+                Console.Write(n + " ");
             }
-            //Imprimindo Invertido
-            Console.WriteLine("\nNúmeros de 25 a 1:");
-            for (int i = pilha.Count - 1; i >= 0; i--)
-            {
-                Console.Write(pilha[i] + " ");
-            }
+            Console.WriteLine();
+
         }
     }
 }
