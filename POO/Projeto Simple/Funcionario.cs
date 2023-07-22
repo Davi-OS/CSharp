c4df73ea27ff32a4b3a7c95ee03f7771686452e8
Davi de Oliveira <davioliveirasanto.work@gmail.com>
2023-07-22 12:39:15 -0300

    subindo  alteraçoes simples

--- POO/Projeto Simple/Funcionario.cs
+++ POO/Projeto Simple/Funcionario.cs
@@ -1,13 +1,15 @@
 using System;
 namespace OOP
 {
+    // não pode ser instanciado;
 
-
+    // Classes abstract são classes modelos para outras classes (Ex: a classe Funcionario é modelo para a Calsse Gerente.)
     public abstract class Funcionario
     {
         public string nome { get; set; }
         public double salario { get; set; }
 
+        // Reajusta serve como modelo para outras classes filhas.
         public abstract void reajustar();
     }
 }
