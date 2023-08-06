0bff38a8fef93ce38a4227a15a5860b127237d4a
Davi de Oliveira <davioliveirasanto.work@gmail.com>
2023-08-06 17:04:51 -0300

    Primeira API em C#

---
+++ APIs + C#/MinhaPrimeiraAPI/MinhaPrimeiraAPI/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MinhaPrimeiraAPI.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+
+        public string? email { get; set;}
+    }
+}
