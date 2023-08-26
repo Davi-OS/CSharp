82787b89ddc9450ced397f7eef1f9272f0ca5895
Davi de Oliveira <davioliveirasanto.work@gmail.com>
2023-08-26 10:18:13 -0300

    ATT 1 Udemy

---
+++ POO/POO Udemy/exercicios Aulas/ConsoleApp/ConsoleApp1/Entitie/HourContract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entitie
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValeuPerHour { get; set; }
+        public int Hours { get; set; }
+        public HourContract() { }
+
+        public HourContract(DateTime date, double valeuPerHour, int hours)
+        {
+            Date = date;
+            ValeuPerHour = valeuPerHour;
+            Hours = hours;
+        }
+
+        public double totalVelue()
+        {
+            return Hours * ValeuPerHour;
+        }
+    }
+}
---
+++ POO/POO Udemy/exercicios Aulas/ConsoleApp/ConsoleApp1/Entitie/Worker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.Entitie.Enums;
+namespace ConsoleApp1.Entitie
+{
+    internal class Worker
+    {
+
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Departament Departament { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+        public Worker()
+        {
+        }
+        public Worker(string name, WorkerLevel level, double baseSalary, Departament departament)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void addContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void removeContract(HourContract contract) { Contracts.Remove(contract); }
+
+        public double income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract item in Contracts)
+            {
+                if (item.Date.Year == year && item.Date.Month == month)
+                {
+                    sum += item.totalVelue();
+                }
+            }
+            return sum;
+        }
+    }
+}
