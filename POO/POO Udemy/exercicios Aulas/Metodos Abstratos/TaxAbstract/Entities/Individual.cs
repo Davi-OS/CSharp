96fb855847fa8c4d91486b387eb3b3c4a66bee07
Davi de Oliveira <davioliveirasanto.work@gmail.com>
2023-09-03 21:50:43 -0300

    Subindo estudos desta semana

---
+++ POO/POO Udemy/exercicios Aulas/Metodos Abstratos/TaxAbstract/Entities/Individual.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaxAbstract.Entities.Abstracts;
+using TaxAbstract.Entities.Interfaces;
+
+namespace TaxAbstract.Entities
+{
+    class Individual : Count
+    {
+        public double HealthExpenditures { get; set; }
+        public Individual(string name, double income, double healthExpenditures)
+        {
+            this.Name = name;
+            this.AnualIncome = income;
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double TaxesToPay()
+        {
+            double total = 0;
+            if (this.AnualIncome < 20000)
+            {
+                total = this.AnualIncome * 0.15;
+            }
+            else
+            {
+                total = this.AnualIncome * 0.25;
+            }
+
+            total -= HealthExpenditures / 2;
+
+            return total;
+
+        }
+    }
+}
---
+++ POO/POO Udemy/exercicios Aulas/MetodosAbstratos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MetodosAbstratos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
