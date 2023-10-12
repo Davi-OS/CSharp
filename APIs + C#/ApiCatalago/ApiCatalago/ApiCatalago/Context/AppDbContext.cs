bed41eec5a97d1fc349bc528855e0fd649716dc4
Davi de Oliveira <davioliveirasanto.work@gmail.com>
2023-10-12 01:20:01 -0300

    indo pro Notebook

---
+++ APIs + C#/ApiCatalago/ApiCatalago/ApiCatalago/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCatalago.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalago.Context;
+
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
---
+++ APIs + C#/ApiCatalago/ApiCatalago/ApiCatalago/Models/Produto.cs
@@ -0,0 +1,42 @@
+﻿namespace ApiCatalago.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+[Table("Produtos")]
+public class Produto
+{
+    //Classes que  possuem somente propiedades são chamadas de anemicas
+
+    //Chave Primaria para o EF
+    [Key]
+    public int ProdutoId { get; set; }
+    //
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? Descricao { get; set; }
+
+    [Required]
+    [Column(TypeName ="Decimal (10,2)")]
+
+    public decimal Preco { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+
+    public float Estoque { get; set; }
+
+    public DateTime DataCadastro { get; set; }
+
+
+    // cada produto possui uma categoria e uma categoria ID
+    public int CategoriaId { get; set; }
+
+    public Categoria? Categoria { get; set; }
+
+}
